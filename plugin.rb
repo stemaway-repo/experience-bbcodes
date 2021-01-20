acb4fa8491371f400a8fb0e29b8bee4a01a50ecd
Jay Pfaffman <pfaffman@gmail.com>
2021-01-20 11:46:42 -0800

    add job/education to toolbar

--- plugin.rb
+++ plugin.rb
@@ -8,12 +8,5 @@ enabled_site_setting :formattingtlb_enabled
 
 register_asset 'stylesheets/formatting.scss'
 
-register_svg_icon "fa-underline" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-indent" if respond_to?(:register_svg_icon)
-register_svg_icon "far-image" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-left" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-center" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-right" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-align-justify" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-palette" if respond_to?(:register_svg_icon)
-register_svg_icon "fa-font" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-university" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-building" if respond_to?(:register_svg_icon)
